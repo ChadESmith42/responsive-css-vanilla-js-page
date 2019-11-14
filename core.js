afe0f705696c6e2ef47f107911f344349a072be0
Chad Smith <Chad.Smith@BlueKC.com>
2019-11-14 12:32:13 -0600

    cleaned up comments

--- core.js
+++ core.js
@@ -1,30 +1,26 @@
-//This function adds the Comment Count to the count_badge on each
-//article. Ideally, this would be done via Angular/AJAX. This gives
-//some "dynamic content" which made testing less boring.
-//This function is called on page load via the <body> tag;
+// This function adds the Comment Count to the count_badge on each
+// article. Ideally, this would be done via an async API call. This gives
+// some "dynamic content" which made testing less boring.
+// This function is called on page load via the <body> tag;
 function addCountBadgeID() {
-    //Select all NODES with Class="count_badge";
+    // Select all NODES with Class="count_badge";
     var x = document.getElementsByClassName("count_badge");
 
-    //If any are found ...
+    // If any are found ...
     if (x.length) {
-        //Loop through each NODE and assign an ID (count_badge + index)
-        //expect results as id="count_badge3";
+        // Loop through each NODE and assign an ID (count_badge + index)
+        // expect results as id="count_badge3";
         for (let index = 0; index < x.length; index++) {
             x[index].innerHTML = Math.floor(Math.random() * 15);
         }
     }
-    //Using the console.log() to provide some feedback;
+    // Using the console.log() to provide some feedback;
     if (x.length == 0) {
         console.log("It didn't work.");
     }
 }
 
-//Just showing that I can add dynamic JS content. Pointless as
-//I would either create content using Angular/AJAX or hard-code
-//a single page like this.
-
-//Create an object of article details;
+// Create an object of article details;
 let authors = [{
     name: "Hingle McKringleBerry",
     date: "4/17/2018"
@@ -36,10 +32,9 @@ let authors = [{
     date: "1/14/2018"
 }];
 
-//Create a function to add the details to the document:
-
+// Create a function to add the details to the document:
 function addArticleDetails() {
-    //Select all NODES with Class="article__author"
+    // Select all NODES with Class="article__author"
     var x = document.getElementsByClassName("article__author");
     if (x.length) {
         for (let index = 0; index < x.length; index++) {
@@ -55,15 +50,15 @@ function addArticleDetails() {
     }
 }
 
-//This function toggles the menu button for mobile devices and toggles
-//the mobile menu items visibility.
+// This function toggles the menu button for mobile devices and toggles
+// the mobile menu items visibility.
 function toggleMenu(x) {
-    //Toggle the menu button to "show" the menu
+    // Toggle the menu button to "show" the menu
     x.classList.toggle("change");
-    //Toggle the nav-bar-mobile to modify size for new elements;
+    // Toggle the nav-bar-mobile to modify size for new elements;
     document.getElementById("nav-bar-mobile").classList.toggle("change");
-    //The next few lines toggles the (visibility: visible) for menu
-    //items and adds other styling;
+    // The next few lines toggles the (visibility: visible) for menu
+    // items and adds other styling;
     document.getElementById("home-mobile").classList.toggle("change");
     document.getElementById("about-mobile").classList.toggle("change");
     document.getElementById("work-mobile").classList.toggle("change");
@@ -73,42 +68,8 @@ function toggleMenu(x) {
 
 function setActive(x) {
 
-    //Basically, need to turn off the active for the previous link ...
-    //Check the other links, and remove the active class;
-
-    // Refactored this whole function to 6 lines of code below:
-
-    // if (document.getElementById("home").classList.contains("active")) {
-    //     document.getElementById("home").classList.remove("active");
-    // }
-    // if (document.getElementById("about").classList.contains("active")) {
-    //     document.getElementById("about").classList.remove("active");
-    // }
-    // if (document.getElementById("work").classList.contains("active")) {
-    //     document.getElementById("work").classList.remove("active");
-    // }
-    // if (document.getElementById("links").classList.contains("active")) {
-    //     document.getElementById("links").classList.remove("active");
-    // }
-    // if (document.getElementById("contact").classList.contains("active")) {
-    //     document.getElementById("contact").classList.remove("active");
-    // }
-    // if (document.getElementById("home-mobile").classList.contains("active")) {
-    //     document.getElementById("home-mobile").classList.remove("active");
-    // }
-    // if (document.getElementById("about-mobile").classList.contains("active")) {
-    //     document.getElementById("about-mobile").classList.remove("active");
-    // }
-    // if (document.getElementById("work-mobile").classList.contains("active")) {
-    //     document.getElementById("work-mobile").classList.remove("active");
-    // }
-    // if (document.getElementById("links-mobile").classList.contains("active")) {
-    //     document.getElementById("links-mobile").classList.remove("active");
-    // }
-    // if (document.getElementById("contact-mobile").classList.contains("active")) {
-    //     document.getElementById("contact-mobile").classList.remove("active");
-    // }
-
+    // Basically, need to turn off the active for the previous link ...
+    // Check the other links, and remove the active class;
     var locations = [ 'home', 'about', 'work', 'links', 'contact','home-mobile', 'about-mobile', 'work-mobile','links-mobile','contact-mobile'];
     for (let loc of locations) {
         if (document.getElementById(loc).classList.contains("active")) {
@@ -116,35 +77,29 @@ function setActive(x) {
         }
     }
 
-
-    //Change the class for the current link
+    // Change the class for the current link
     x.classList.toggle("active");
     toggleMenu(document.getElementById("nav-bar-toggle"));
-
 }
 
 function scrollToLink(elementId) {
-    //Assign the input to a variable in order to find the coordinates of the target in the
-    //document:
+    // Assign the input to a variable in order to find the coordinates of the target in the
+    // document:
     let location = document.getElementById(elementId)
-    //Get the headerLocation based on <div class="header">
-    //The header contains the sticky element I want at the top of the screen.
+    // Get the headerLocation based on <div class="header">
+    // The header contains the sticky element I want at the top of the screen.
     let headerLocation = document.getElementById("header");
 
-    // console.log("Target top pixel y-coordinate: " + location.offsetTop);
-    // console.log("Header scrollHeight: " + headerLocation.scrollHeight);
-    // console.log("Attempting to scroll to : " + (location.offsetTop - headerLocation.scrollHeight));
-
-    //Create a new variable to include an offset for the header element;
-    //Using the JS functions of offsetTop and scrollHeight to get the right target
-    //location within the document. By using these properties of the DOM, the app
-    //will perform consistently(-ish) across different devices, browsers, and
-    //display resolutions.
-    //Adding an additional 150px based on some experimentation;
+    // Create a new variable to include an offset for the header element;
+    // Using the JS functions of offsetTop and scrollHeight to get the right target
+    // location within the document. By using these properties of the DOM, the app
+    // will perform consistently(-ish) across different devices, browsers, and
+    // display resolutions.
+    // Adding an additional 168px based on some experimentation;
     let topVisible = location.offsetTop - headerLocation.scrollHeight + 168;
 
     window.scrollTo({
-        top: topVisible,
+        top: location.offsetTop - headerLocation.scrollHeight + 168,
         behavior: "smooth"
     });
 };
@@ -156,8 +111,8 @@ function setCopyrightDate() {
 }
 
 
-//This function is called by the <body onload=()> tag in the
-//document;
+//  This function is called by the <body onload=()> tag in the
+//  document;
 function loadPage() {
     this.setCopyrightDate();
     this.addArticleDetails();
